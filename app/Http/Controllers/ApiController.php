f8a5d2a09353633b844af35f59211bae86d3690c
Artyom Nischuk <arttemiuss43@gmail.com>
2020-05-23 15:34:26 +0600

    Add php docblock to api controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,10 +4,17 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
 abstract class ApiController extends Controller
 {
+    /**
+     * Return successful json response.
+     *
+     * @param $body
+     * @return JsonResponse
+     */
     protected function ok($body)
     {
         return response()->json($body, Response::HTTP_OK);
